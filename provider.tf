6c73de78be61d84f8ba83baa00cfadd2e0e0e83c
Sarath kumar S <sarathkumar@local>
2025-10-22 05:51:00 +0000

    jenkins file added

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_files = ["/home/sarathkumar/.aws/credentials"]
+  profile                  = "terraform"
+}
